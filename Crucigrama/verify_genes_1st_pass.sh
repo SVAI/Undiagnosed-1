087a75dcfeb359ed51c6f3dd23d604b4eea95e6e
Rene Lopez <lopezreneb@yahoo.com>
2019-06-10 09:32:26 -0700

    adding all files

--- Crucigrama/verify_genes_1st_pass.sh
+++ Crucigrama/verify_genes_1st_pass.sh
@@ -1,16 +1,16 @@
 cat vep.genes.tsv |grep ALDOB
 cat vep.genes.tsv |grep TTN
 cat vep.genes.tsv |grep GYG1
-cat vep.genes.tsv |grep NFIX
+cat vep.genes.tsv |grep NFIX #No
 cat vep.genes.tsv |grep TTC21B
-cat vep.genes.tsv |grep WDR19
+cat vep.genes.tsv |grep WDR19 #No
 cat vep.genes.tsv |grep EXOSC3
-cat vep.genes.tsv |grep RYR2
-cat vep.genes.tsv |grep SGCB
+cat vep.genes.tsv |grep RYR2 #No
+cat vep.genes.tsv |grep SGCB #No
 cat vep.genes.tsv |grep AMPD1
 cat vep.genes.tsv |grep IL17RC
 cat vep.genes.tsv |grep LRBA
-cat vep.genes.tsv |grep RMRP
+cat vep.genes.tsv |grep RMRP #No
 cat vep.genes.tsv |grep TH
 cat vep.genes.tsv |grep UNC93B1
 cat vep.genes.tsv |grep VPS13B
